d7ae0f742913fe4a850f880e294c6cba5ec2674d
brooksn <mail@brooks.is>
2015-05-08 12:52:27 -0700

    trying things out on POST.new_post

--- test/client.js
+++ test/client.js
@@ -1,27 +1,36 @@
 //https://github.com/hueniverse/hawk
 var Request = require('request');
 var Hawk = require('hawk');
-var url = "http://localhost:3000";
+var url = "http://localhost:3000/new_post/1";
 
 // Client credentials
 
 var credentials = {
-    id: 'testid',
-    key: 'testkey',
+    id: 'l8KxRj2zXSK8oY9VHE4Zd',
+    key: '_wzccdkOhA4ViByy1DmqY',
     algorithm: 'sha256'
 }
 
 // Request options
 
+var post = {
+  type: 'https://tent.io/types/status/v0',
+  content: {
+    text: 'Hello World!'
+  }
+};
 var requestOptions = {
     uri: url,
-    method: 'GET',
-    headers: {}
+    method: 'POST',
+    body: JSON.stringify(post),
+    headers: {
+      "Content-Type": "application/json"
+    }
 };
 
 // Generate Authorization request header
 
-var header = Hawk.client.header(url, 'GET', { credentials: credentials, ext: 'some-app-data' });
+var header = Hawk.client.header(url, requestOptions.method, { credentials: credentials, ext: 'some-app-data' });
 requestOptions.headers.Authorization = header.field;
 
 // Send authenticated request
